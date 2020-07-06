476b207ca0cb5f38fa2068cc9cbf06a3b91f1ccf
eryk1120 <eryk@pop-os.localdomain>
2020-07-07 01:09:01 +0200

    playing around with servers and gpio

---
+++ simple_webserver.py
@@ -0,0 +1,93 @@
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+host_name = '192.168.99.101' 
+host_port = 8000
+
+
+led = 40
+switch = 31
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(switch, GPIO.IN)
+
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
+
+    def do_HEAD(self):
+        """ do_HEAD() can be tested use curl command
+            'curl -I http://server-ip-address:port'
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        """ do_GET() can be tested using curl command
+            'curl http://server-ip-address:port'
+        """
+        html = '''
+           <html>
+           <body style="width:960px; margin: 20px auto;">
+           <h1>Welcome to my Raspberry Pi</h1>
+           <p>Current GPU temperature is {}</p>
+           <form action="/" method="POST">
+               Turn LED :
+               <input type="submit" name="submit" value="On">
+               <input type="submit" name="submit" value="Off">
+           </form>
+           </body>
+           </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        """ do_POST() can be tested using curl command
+            'curl -d "submit=On" http://server-ip-address:port'
+        """
+        content_length = int(self.headers['Content-Length'])  # Get the size of data
+        post_data = self.rfile.read(content_length).decode("utf-8")  # Get the data
+        post_data = post_data.split("=")[1]  # Only keep the value
+
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(18, GPIO.OUT)
+
+        if post_data == 'On':
+            for i in range(10):
+                GPIO.output(led, GPIO.HIGH)
+                time.sleep(0.2)
+                GPIO.output(led, GPIO.LOW)
+                time.sleep(0.2)
+                print('Switch status = ', GPIO.input(switch))
+
+        else:
+            GPIO.output(18, GPIO.LOW)
+        print("LED is {}".format(post_data))
+        self._redirect('/')  # Redirect back to the root url
+
+
+if __name__ == '__main__':
+    http_server = HTTPServer((host_name, host_port), MyServer)
+    print("Server Starts - %s:%s" % (host_name, host_port))
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
